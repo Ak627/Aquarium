04e6b95ddd77dc0c2a56e4c1586d8d8f0c6b98a2
Ak627 <91678317+Ak627@users.noreply.github.com>
2022-09-01 11:55:52 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((700,500))
+
+fx = 350
+fy = 250
+fVx = 2
+fVy = 2
+clock = pygame.time.Clock()
+doExit = False
+
+while not doExit:
+  clock.tick(60)
+  if fx < 60 or fx + 20 > 640:
+    fVx *= -1
+  if fy < 100 or fy + 20 > 440:
+    fVy *= -1
+  
+  fx += fVx
+  fy += fVy
+  screen.fill((51, 42, 100))
+  pygame.draw.rect(screen, (0, 0, 255), (50, 100, 600, 350))
+  pygame.draw.rect(screen, (105,56,0), (40, 450, 620, 100))
+  pygame.draw.rect(screen, (255, 255, 255), (50, 50, 600, 400), 10)
+  pygame.draw.rect(screen, (0,0,0), (50, 50, 600, 400), 5)
+  pygame.draw.rect(screen, (0,0,0), (45, 50, 610, 25))
+  pygame.draw.rect(screen, (255, 137, 0), (fx, fy, 20, 20))
+
+  pygame.display.flip()
+pygame.quit()
